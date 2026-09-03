26f8cff9e872f90ef23db9197164216be5abe11f
madeline-gwin <146005253+madeline-gwin@users.noreply.github.com>
2026-09-03 16:19:54 -0400

    Update star_indexing.sh

--- shell_scripts/star_indexing.sh
+++ shell_scripts/star_indexing.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+#SBATCH --job-name=STAR_Hap2_GTF        # job name
+#SBATCH --partition=capitulab           # partition
+#SBATCH --ntasks=1                      # number of tasks across all nodes
+#SBATCH --cpus-per-task=1               # number of cpus per task
+#SBATCH --mem=35GB                      # total memory requested
+#SBATCH --time=72:00:00                 # Run time (D-HH:MM:SS)
+#SBATCH --output=job-%j.out             # Output file. %j is replaced with job ID
+#SBATCH --error=job-%j.err              # Error file. %j is replaced with job ID
+#SBATCH --mail-type=ALL                 # will send email for begin,end,fail
+#SBATCH --mail-user=magwin@clemson.edu
+
 #What file format is your genome annotation file? (GTF or GFF3)
-ANNOTATION_FORMAT="GFF3"
+ANNOTATION_FORMAT="GTF"
 
 #Where is the Genome Annotation file (GFF3 or GTF)?
-GEN_ANN="/scratch/magwin/genome/Annotation_files/Tagetes_erecta.scaffold.genes.v2.gff3"
+GEN_ANN="/scratch/magwin/BCY_floret_dev_analysis/genomes/Hap2_GTF/BidensCY_H2_genes.gtf"
 
 #Where is the Genome FASTA file?
-GEN_FASTA="/scratch/magwin/genome/FASTA_files/Tagetes_erecta.scaffold.genome.v2.fa"
+GEN_FASTA="/scratch/magwin/BCY_floret_dev_analysis/genomes/Hap2_GTF/BidensCY_H2.fasta"
 
 #Where do you want the files for your genome index?
-GEN_DIR="/scratch/magwin/genome/genome_indices/Marigold"
+GEN_DIR="/scratch/magwin/BCY_floret_dev_analysis/genomes/Hap2_GTF/genome_indices/"
+
+# mkake output directory, if it doesnt exist
+mkdir -p $GEN_DIR
 
 #Specify the length of genomic sequence to be used in constructing the splice junctions database.
 #This length should be equal to ReadLength-1, where ReadLength is the length of reads
@@ -20,7 +34,8 @@ SPLICE_JUN=149
 set -o pipefail
 
 # read in the star module
-ml STAR
+module load biocontainers
+module load star
 
 # Define Exon Parent Gene and Transcript Tags
 if [ "$ANNOTATION_FORMAT" == "GFF3" ]; then
@@ -48,4 +63,3 @@ echo "Beginning indexing"
 --sjdbGTFtagExonParentGene $GENE_TAG \
 --sjdbGTFfile $GEN_ANN \
 --sjdbOverhang $SPLICE_JUN \
-
